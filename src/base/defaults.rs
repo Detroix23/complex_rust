6a0418424633d5909d9c44547c8fe92a1608731e
Detroix23 <detroix23@gmail.com>
2026-04-14 14:09:47 +0200

    Updated 'Real' to 'f64'

--- src/base/defaults.rs
+++ src/base/defaults.rs
@@ -4,7 +4,7 @@
 /// Define the current type in use.
 /// 
 /// Use primitive: `Real` is used in `as` casts.
-pub type Real = f32;
+pub type Real = f64;
 
 /// ULPS level in `float_cmp` for the whole crate. 
-pub(crate) const ULPS: i32 = 2;
+pub(crate) const ULPS: i64 = 2;
